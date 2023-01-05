f223115f0b0b759a1873b6115dbec9135276b3dc
Ari625 <arir06101@gmail.com>
2023-01-05 16:56:11 +0700

    kamis 05-01-2023

--- trivia-website/resources/views/login/index.blade.php
+++ trivia-website/resources/views/login/index.blade.php
@@ -1,16 +1,16 @@
 @extends('layouts.sign')
 
 @section('container')
-   <main class="form-signin w-60 m-auto">
+   <main class="form-signin w-80 m-auto">
       <form>
       <h1 class="h3 mb-3 fw-normal text-center text-primary text-shadow">LOGIN</h1>
    
       <div class="form-floating">
-         <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+         <input type="email" class="form-control form-control-sm" id="floatingInput" placeholder="name@example.com">
          <label for="floatingInput">Email address</label>
       </div>
       <div class="form-floating mt-1">
-         <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+         <input type="password" class="form-control form-control-sm" id="floatingPassword" placeholder="Password">
          <label for="floatingPassword">Password</label>
       </div>
    
